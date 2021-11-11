fd26344174dd51091fd9008d1afc7bea583bff1e
Tatr1 <ttttt_68@mail.ru>
2021-11-12 00:06:27 +0300

    added app-order

--- src/test/java/ru/netology/testSelenium.java
+++ src/test/java/ru/netology/testSelenium.java
@@ -7,14 +7,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class testSelenium {
-    private WebDriver driver;
+    WebDriver driver;
 
     @BeforeAll
     static void setupClass() {
@@ -37,7 +40,7 @@ public class testSelenium {
     }
 
     @Test
-    void shouldTestSendFormS1() {
+    void shouldTestSendFormSelenium() {
         driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Петров-Водкин Кузьма");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+73222332222");
